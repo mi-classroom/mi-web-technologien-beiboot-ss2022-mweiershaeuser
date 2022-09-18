714e18c720d81be17fc244a73e1044976514c8bd
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2022-09-19 00:06:15 +0200

    added relations and cda links to artwork details

--- frontend/src/app/explore/timeline/components/masterpiece-details/masterpiece-details.component.ts
+++ frontend/src/app/explore/timeline/components/masterpiece-details/masterpiece-details.component.ts
@@ -1,18 +1,77 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+} from '@angular/core';
 import Artwork from 'src/app/explore/models/artwork.model';
+import * as constants from '../../constants';
 
 @Component({
   selector: 'app-masterpiece-details',
   templateUrl: './masterpiece-details.component.html',
   styleUrls: ['./masterpiece-details.component.scss'],
 })
-export class MasterpieceDetailsComponent implements OnInit {
+export class MasterpieceDetailsComponent implements OnChanges {
+  consts = constants;
+
   @Input() artwork!: Artwork;
+  @Input() artworks!: Artwork[];
   @Output() close: EventEmitter<void> = new EventEmitter();
+
+  partOfWork: Artwork[] = [];
+  belongsTo: Artwork[] = [];
+  similarToOrRelatedInContentTo: Artwork[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnChanges(): void {
+    this.getRelations();
+  }
+
+  getRelations() {
+    this.artwork.relations.forEach((relation) => {
+      let relationArtwork;
+      switch (relation.type) {
+        case 'PART_OF_WORK':
+          relationArtwork = this.artworks.find(
+            (a) => a.inventoryNumber === relation.inventoryNumber
+          );
+          if (relationArtwork) {
+            this.partOfWork.push(relationArtwork);
+          }
+          break;
+
+        case 'BELONGS_TO':
+          relationArtwork = this.artworks.find(
+            (a) => a.inventoryNumber === relation.inventoryNumber
+          );
+          if (relationArtwork) {
+            this.belongsTo.push(relationArtwork);
+          }
+          break;
+
+        case 'SIMILAR_TO':
+          relationArtwork = this.artworks.find(
+            (a) => a.inventoryNumber === relation.inventoryNumber
+          );
+          if (relationArtwork) {
+            this.similarToOrRelatedInContentTo.push(relationArtwork);
+          }
+          break;
+
+        case 'RELATED_IN_CONTENT_TO':
+          relationArtwork = this.artworks.find(
+            (a) => a.inventoryNumber === relation.inventoryNumber
+          );
+          if (relationArtwork) {
+            this.similarToOrRelatedInContentTo.push(relationArtwork);
+          }
+          break;
+      }
+    });
+  }
 
   onClose() {
     this.close.emit();
--- frontend/src/app/explore/timeline/constants.ts
+++ frontend/src/app/explore/timeline/constants.ts
@@ -1,3 +1,5 @@
+export const cdaBaseUrl = 'https://lucascranach.org/de/';
+
 export const xStart = -1.2;
 export const yStart = -1;
 export const zStart = 0;
