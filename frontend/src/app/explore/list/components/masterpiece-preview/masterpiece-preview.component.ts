aceeb5d864ed05db2309813515d428b5c648810d
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2022-09-20 00:15:40 +0200

    beautified list view

--- frontend/src/app/explore/list/components/masterpiece-preview/masterpiece-preview.component.ts
+++ frontend/src/app/explore/list/components/masterpiece-preview/masterpiece-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import * as constants from '../../constants';
 import Artwork from 'src/app/explore/models/artwork.model';
 
 @Component({
@@ -7,5 +8,7 @@ import Artwork from 'src/app/explore/models/artwork.model';
   styleUrls: ['./masterpiece-preview.component.scss'],
 })
 export class MasterpiecePreviewComponent {
+  consts = constants;
+
   @Input() artwork?: Artwork;
 }
---
+++ frontend/src/app/explore/list/constants.ts
@@ -0,0 +1,1 @@
+export const cdaBaseUrl = 'https://lucascranach.org/de/';
